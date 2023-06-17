16a2fa89c5e8dcc2588d647e32e00232bc6ef284
Ahsan Malik <112468189+ahsanmalik9@users.noreply.github.com>
2023-06-17 23:48:03 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const mongoDB = require("./db")//calling db.js
-mongoDB(); // calling function
+const mongoDB = require("./db")
+mongoDB(); 
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -13,14 +13,11 @@ app.use((req,res,next)=>{
   );
   next();
 })
-
-//routes ki madad sy endpoint bnaty hain which good way instead of writing code as a whole
-// app.use is the middleware (Middleware means 2 code k drmyan 1 or code run krwana)
 app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));//this the endpoint 
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
-app.get('/', (req, res) => {       //app.get is one of the endpoint we created
+app.get('/', (req, res) => {      
   res.send('Hello World jee!')
 })
 
