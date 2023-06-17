d350d5ac6786a01119c0c584e300c3eee3246a4c
Ahsan Malik <112468189+ahsanmalik9@users.noreply.github.com>
2023-06-17 23:47:22 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,18 @@
 // connecting backend with database mongodb using the mongoose
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://mernproject1:882229mernproject1@cluster0.0kvoegm.mongodb.net/foodwebmern?retryWrites=true&w=majority' //? is sy database ka name likhna hai
+const mongoURI = 'mongodb+srv://mernproject1:882229mernproject1@cluster0.0kvoegm.mongodb.net/foodwebmern?retryWrites=true&w=majority' 
 
-// above link is in -> atlas -> database -> connect -> connect your application -> copy link and change password 
-const mongoDB = () => { //funtion
-     mongoose.connect(mongoURI, {useNewUrlParser: true},async(err,result) => { // ()=> arrow function
+const mongoDB = () => { 
+     mongoose.connect(mongoURI, {useNewUrlParser: true},async(err,result) => { 
         if(err) console.log("---",err)
         else{
-        console.log("connected ");//display output
+        console.log("connected ");
         
         //Read / Fetch data from database
         const fetched_data = await mongoose.connection.db.collection("food_items");
-        fetched_data.find({}).toArray( async function(err, data){ // empty curly k through sara data fetch kr ley ga
+        fetched_data.find({}).toArray( async function(err, data){ 
 
-            const foodCategory = await mongoose.connection.db.collection("foodCategory"); // food item and category ko aik sth call krwa rhy
+            const foodCategory = await mongoose.connection.db.collection("foodCategory"); 
             foodCategory.find({}).toArray(function (err, catData){
                 if(err)console.log(err);
                 else {
@@ -23,13 +22,6 @@ const mongoDB = () => { //funtion
                 }
 
             })
-            //     if(err)console.log(err);
-        //    else {
-
-        //     global.food_items = data;
-        //     //console.log(global.food_items) comment short key (cntrl + k + c)
-            
-        //    }
         })        
         
     }
